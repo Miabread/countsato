52c1d46b6aeddd7ad9e1a58ed300c85dc6eb3b46
Miabread <miabreadloaf@gmail.com>
2024-02-03 15:33:57 -0500

    Add example env file

--- src/deploy.ts
+++ src/deploy.ts
@@ -3,6 +3,6 @@ import { commands } from './commands';
 
 await new REST()
     .setToken(process.env.DISCORD_TOKEN ?? '')
-    .put(Routes.applicationGuildCommands(process.env.CLIENT_ID ?? '', process.env.DEV_GUILD_ID ?? ''), {
+    .put(Routes.applicationGuildCommands(process.env.CLIENT_ID ?? '', process.env.DEPLOY_GUILD_ID ?? ''), {
         body: commands.map((command) => command.data.toJSON()),
     });
--- src/events/meow.ts
+++ src/events/meow.ts
@@ -2,10 +2,9 @@ import { Events } from 'discord.js';
 import { client } from '..';
 
 const meowRegex = /(m+(e+|r+)o+w+)|(n+y+a+)|(m+r+p+)/;
-const meowGuilds = process.env.MEOW_GUILDS?.split(',') ?? [];
 
 client.on(Events.MessageCreate, async (message) => {
-    if (message.inGuild() && !meowGuilds.includes(message.guildId)) return;
+    return;
     if (!meowRegex.test(message.content)) return;
     await message.react('🐱');
 });
