7e1d06b59883c24bd6c762e2582e83a2ff891782
Miabread <miabreadloaf@gmail.com>
2024-02-03 14:54:39 -0500

    Add countingChannel as a gamerule

--- src/commands/gamerule.ts
+++ src/commands/gamerule.ts
@@ -1,4 +1,4 @@
-import { ApplicationCommandOptionBase, PermissionFlagsBits, SlashCommandBuilder } from 'discord.js';
+import { ChannelType, PermissionFlagsBits, SlashCommandBuilder } from 'discord.js';
 import { commands } from '.';
 import { prisma } from '../util';
 
@@ -9,9 +9,14 @@ const gamerules = {
         field: 'allowDoubleCounting',
     },
     grace_milliseconds: {
-        description: 'grace_milliseconds',
+        description: 'Amount of milliseconds after last valid count before invalid counts ruin the count',
         type: 'Integer',
         field: 'graceMilliseconds',
+    },
+    counting_channel: {
+        description: 'The channel the bot listens for counting',
+        type: 'Channel',
+        field: 'countingChannel',
     },
 } as const;
 
@@ -22,14 +27,29 @@ const data = new SlashCommandBuilder()
     .setDMPermission(false);
 
 for (const [key, gamerule] of Object.entries(gamerules)) {
-    data.addSubcommand((sub) =>
-        sub
-            .setName(key)
-            .setDescription(gamerule.description)
-            [`add${gamerule.type}Option` as const]((option: any) =>
+    data.addSubcommand((sub) => {
+        sub.setName(key).setDescription(gamerule.description);
+
+        if (gamerule.type === 'Boolean') {
+            sub.addBooleanOption((option) =>
                 option.setName('value').setDescription('Leave blank to see current value').setRequired(false),
-            ),
-    );
+            );
+        } else if (gamerule.type === 'Integer') {
+            sub.addIntegerOption((option) =>
+                option.setName('value').setDescription('Leave blank to see current value').setRequired(false),
+            );
+        } else if (gamerule.type === 'Channel') {
+            sub.addChannelOption((option) =>
+                option
+                    .setName('value')
+                    .setDescription('Leave blank to see current value')
+                    .setRequired(false)
+                    .addChannelTypes(ChannelType.GuildText),
+            );
+        }
+
+        return sub;
+    });
 }
 
 commands.push({
@@ -49,7 +69,14 @@ commands.push({
         const gamerule = gamerules[key as keyof typeof gamerules];
         if (!gamerule) throw new Error('gamerule');
 
-        const value = interaction.options[`get${gamerule.type}`]('value', false);
+        const value =
+            gamerule.type === 'Boolean'
+                ? interaction.options.getBoolean('value', false)
+                : gamerule.type === 'Integer'
+                ? interaction.options.getInteger('value', false)
+                : gamerule.type === 'Channel'
+                ? interaction.options.getChannel('value', false, [ChannelType.GuildText])?.id
+                : null;
 
         if (value === null) {
             await interaction.reply(`Currently ${key} is ${data.allowDoubleCounting}`);
